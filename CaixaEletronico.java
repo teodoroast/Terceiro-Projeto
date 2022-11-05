73afce63b080f8f2ecfb0c1addebf078cd3193b0
teodoroast <adriano.teodoroast@gmail.com>
2022-11-04 23:59:13 -0300

    push-main3

--- CaixaEletronico.java
+++ CaixaEletronico.java
@@ -11,8 +11,6 @@ public class CaixaEletronico {
         //double saldo = 25.0;
         //double valorSolicitado = 17.0;
 
-
-
         if(valorSolicitado < saldo)
         saldo = saldo - valorSolicitado;
 
