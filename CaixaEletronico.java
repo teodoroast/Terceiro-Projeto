eb8ef6ae70b682bb1e4a3d3c76ed9e787a609c01
teodoroast <adriano.teodoroast@gmail.com>
2022-11-04 23:51:45 -0300

    primeiro commit

---
+++ CaixaEletronico.java
@@ -0,0 +1,17 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        
+        //double saldo = 25.0;
+        //double valorSolicitado = 17.0;
+
+        if(valorSolicitado < saldo)
+        saldo = saldo - valorSolicitado;
+
+        System.out.println(saldo);
+    }
+}
